53f435c798f22342abd8312db98bf5262aeccd39
Brandon <WalkingMyPetRock@gmail.com>
2024-10-28 21:56:14 -0400

    made autonomous for two red negative

---
+++ src/routes/three_red_negative.cpp
@@ -0,0 +1,34 @@
+#include "../movement.hpp"
+#include "../helper_functions.hpp"
+#include "vex.h"
+//PID Straight and turn arguments:
+// MoveEncoderPID(TestPara, motor speed, encoder travel distance (inches), time to full speed(sec), relative heading(to starting position), braking?)
+// TurnMaxTimePID(TestPara, Desired Heading -180 to 180, time out to calculate turn, Braking?)
+// MoveTimePID(TestPara, motor speed, time traveled (sec), time to full speed, heading, false);
+
+// make sure that heading stays the same between turns
+// or not if u think you can do it without
+
+void three_red_negative() 
+{
+    PIDDataSet TestPara={1.5,0.1,0.15}; // this is important and DONT TOUCH THIS
+
+    MoveEncoderPID(TestPara, 100, -23, 0.4, 0,true); // this just moves the bot backwards
+
+    MoveEncoderPID(TestPara, 40, -5, 0.4, 0,true); // keep the travel distance as a negative and change motor speed to negative to go forward
+    // also travel distance isnt really in inches
+    // i heard that just get the number u put in and divide by two for the actual distance
+
+    wait(250,msec); // put some waits in between code if u want 
+
+    Clamp.set(true); // true turns on the clamp, false turns it off
+
+    TurnMaxTimePID(TestPara, 90, 0.9, true); // this turns the bot
+    // the heading is also relative to starting position
+    // so its just turning to 90 degrees relative to starting position
+    // instead of actually turning 90 degrees
+    // also this turns right (i think)
+    // so -90 turns left 90 degrees
+
+    RunRoller(100); // sets intake at ur desired speed. positive integer will intake it, negative will outake
+}
--- src/routes/two_red_negative.cpp
+++ src/routes/two_red_negative.cpp
@@ -10,7 +10,7 @@ void two_red_negative()
 {
     PIDDataSet TestPara={1.5,0.1,0.15};
 
-    MoveEncoderPID(TestPara, 100, -23, 0.4, 0,true); // drives most of the way to mogo
+    MoveEncoderPID(TestPara, 100, -18, 0.4, 0,true); // drives most of the way to mogo
     MoveEncoderPID(TestPara, 40, -5, 0.4, 0,true); // goes rest of the way to mogo
 
     wait(250,msec);
@@ -20,17 +20,53 @@ void two_red_negative()
 
     wait(250,msec);
     TurnMaxTimePID(TestPara, 90, 1, true); // turns to face stacked rings
-    wait(250,msec);
-    MoveEncoderPID(TestPara, -100, -18, 0.4, 90 ,true); // drives towards stacked rings
-    wait(250,msec);
-    MoveEncoderPID(TestPara, 100, -4, 0.4, 90 ,true); // drives away from stacked rings
-    wait(4,sec);
-    RunRoller(-100); // spits out any potential opponent rings
-    MoveEncoderPID(TestPara, 100, -5, 0.4, 90 ,true); // fully removes the rings from the bot
+    wait(200,msec);
+    MoveEncoderPID(TestPara, -100, -25, 0.4, 90 ,true); // drives towards stacked rings
+    wait(150,msec);
+    MoveEncoderPID(TestPara, 100, -65, 0.4, 110, true);
+    wait(200,msec);
+    Clamp.set(false); // deactivates the clamp
+    wait(100,msec);
+    MoveEncoderPID(TestPara, -100, -7, 0.4, 110, true);
+    wait(150, msec);
+    TurnMaxTimePID(TestPara, 50, 1, true);
+    wait(150, msec);
+    MoveEncoderPID(TestPara, 50, -23, 0.4, 45, true);
+    wait(100, msec);
+    Clamp.set(true); // activates the clamp
+    wait(100, msec);
+    MoveEncoderPID(TestPara, 100, -5, 0.4, 45, true);
+    wait(100, msec);
+    TurnMaxTimePID(TestPara, -90, 1, true);
+    wait(150,msec);
+    MoveEncoderPID(TestPara, -100, -18, 0.4, -90, true);
+    wait(100, msec);
+    MoveEncoderPID(TestPara, 100, -15, 0.4, -90, true);
+    wait(100, msec);
+    TurnMaxTimePID(TestPara, -45, 1, true);
+    RunRoller(0);
+    wait(50, msec);
+    MoveEncoderPID(TestPara, 100, -7, 0.4, -45, true);
+    //MoveEncoderPID(TestPara, 100, -15, 0.4, -135, true);
+    //wait(100, msec);
+    //TurnMaxTimePID(TestPara, 90, 1, true);
+    //MoveEncoderPID(TestPara, 100, -7, 0.4, 90, true);
+    
+    //TurnMaxTimePID(TestPara, 185, 1, true); // turns toward center ring
+    //wait(250,msec);
+    //MoveEncoderPID(TestPara, -50, -8, 0.4, 183, true); // drives towards the center rings
+    //wait(200,msec);
+    //MoveEncoderPID(TestPara, -100, 10, 0.4, 183, true);
+    //MoveEncoderPID(TestPara, 100, -4, 0.4, 90 ,true); // drives away from stacked rings
+    //wait(4,sec);
+    //RunRoller(-100); // spits out any potential opponent rings
+    //MoveEncoderPID(TestPara, 100, -5, 0.4, 90 ,true); // fully removes the rings from the bot
 
     // this stuff down here just makes the bot touch the ladder
 
     // TurnMaxTimePID(TestPara, 45, 1, true);
     // RunRoller(0);
-    // MoveEncoderPID(TestPara, 100, -6, 0.4, 45 ,true);
+    //
+    
+    //MoveEncoderPID(TestPara, 100, -6, 0.4, 45 ,true);
 }
