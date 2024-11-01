8ee326747b2f83e6b52e19abb492664c5f6c9b3b
Aiden Wu <aaiden.w@Aidens-MBP.hitronhub.home>
2024-10-31 21:01:01 -0400

    auton for 3 ring

--- src/routes/three_red_negative.cpp
+++ src/routes/three_red_negative.cpp
@@ -18,36 +18,75 @@ void three_red_negative()
 
     wait(250,msec);
     Clamp.set(true); // activates clamp
-    wait(250,msec);  
+    wait(150,msec);  
     RunRoller(100); // activates intake
 
-    wait(250,msec);
-    TurnMaxTimePID(TestPara, 90, 1, true); // turns to face stacked rings
+        wait(250,msec);
+    TurnMaxTimePID(TestPara, 135, 1, true); // turns to face rings
     wait(200,msec);
-    MoveEncoderPID(TestPara, -100, -25, 0.4, 90 ,true); // drives towards stacked rings
-    wait(150,msec);
-    MoveEncoderPID(TestPara, 100, -65, 0.4, 110, true);
-    wait(200,msec);
-    Clamp.set(false); // deactivates the clamp
+    MoveEncoderPID(TestPara, -100, -25, 0.4, 135 ,true); // drives towards rings
     wait(100,msec);
-    MoveEncoderPID(TestPara, -100, -7, 0.4, 110, true);
+    MoveEncoderPID(TestPara, -120, -5, 0.4, 90, true);
+    MoveEncoderPID(TestPara, 120, -4, 0.4, 90, true);
     wait(150, msec);
-    TurnMaxTimePID(TestPara, 50, 1, true);
-    wait(150, msec);
-    MoveEncoderPID(TestPara, 50, -23, 0.4, 45, true);
+    TurnMaxTimePID(TestPara, 0, 1, false);
     wait(100, msec);
-    Clamp.set(true); // activates the clamp
+    MoveEncoderPID(TestPara, -100, -25, 0.4, 0, true);
     wait(100, msec);
-    MoveEncoderPID(TestPara, 100, -5, 0.4, 45, true);
-    wait(100, msec);
-    TurnMaxTimePID(TestPara, -90, 1, true);
-    wait(150,msec);
-    MoveEncoderPID(TestPara, -100, -18, 0.4, -90, true);
-    wait(100, msec);
-    MoveEncoderPID(TestPara, 100, -15, 0.4, -90, true);
-    wait(100, msec);
-    TurnMaxTimePID(TestPara, -45, 1, true);
-    RunRoller(0);
-    wait(50, msec);
-    MoveEncoderPID(TestPara, 100, -7, 0.4, -45, true);
+    TurnMaxTimePID(TestPara, 35, 1, false);
+    MoveEncoderPID(TestPara, 100, -10, 0.4, 35, false);
+    // wait(200,msec);
+    // Clamp.set(false); // deactivates the clamp
+    // wait(100,msec);
+    // MoveEncoderPID(TestPara, -100, -7, 0.4, 110, true);
+    // wait(150, msec);
+    // TurnMaxTimePID(TestPara, 50, 1, true);
+    // wait(150, msec);
+    // MoveEncoderPID(TestPara, 50, -23, 0.4, 45, true);
+    // wait(100, msec);
+    // Clamp.set(true); // activates the clamp
+    // wait(100, msec);
+    // MoveEncoderPID(TestPara, 100, -5, 0.4, 45, true);
+    // wait(100, msec);
+    // TurnMaxTimePID(TestPara, -90, 1, true);
+    // wait(150,msec);
+    // MoveEncoderPID(TestPara, -100, -18, 0.4, -90, true);
+    // wait(100, msec);
+    // MoveEncoderPID(TestPara, 100, -15, 0.4, -90, true);
+    // wait(100, msec);
+    // TurnMaxTimePID(TestPara, -45, 1, true);
+    // RunRoller(0);
+    // wait(50, msec);
+    // MoveEncoderPID(TestPara, 100, -7, 0.4, -45, true);
+
+    // old code, making new one above
+    // wait(250,msec);
+    // TurnMaxTimePID(TestPara, 90, 1, true); // turns to face stacked rings
+    // wait(200,msec);
+    // MoveEncoderPID(TestPara, -100, -25, 0.4, 90 ,true); // drives towards stacked rings
+    // wait(150,msec);
+    // MoveEncoderPID(TestPara, 100, -65, 0.4, 110, true);
+    // wait(200,msec);
+    // Clamp.set(false); // deactivates the clamp
+    // wait(100,msec);
+    // MoveEncoderPID(TestPara, -100, -7, 0.4, 110, true);
+    // wait(150, msec);
+    // TurnMaxTimePID(TestPara, 50, 1, true);
+    // wait(150, msec);
+    // MoveEncoderPID(TestPara, 50, -23, 0.4, 45, true);
+    // wait(100, msec);
+    // Clamp.set(true); // activates the clamp
+    // wait(100, msec);
+    // MoveEncoderPID(TestPara, 100, -5, 0.4, 45, true);
+    // wait(100, msec);
+    // TurnMaxTimePID(TestPara, -90, 1, true);
+    // wait(150,msec);
+    // MoveEncoderPID(TestPara, -100, -18, 0.4, -90, true);
+    // wait(100, msec);
+    // MoveEncoderPID(TestPara, 100, -15, 0.4, -90, true);
+    // wait(100, msec);
+    // TurnMaxTimePID(TestPara, -45, 1, true);
+    // RunRoller(0);
+    // wait(50, msec);
+    // MoveEncoderPID(TestPara, 100, -7, 0.4, -45, true);
 }
